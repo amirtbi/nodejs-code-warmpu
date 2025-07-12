ecbda27ffe5041166be5fae7ac0cb17978202eff
Amirhosein Torabi <ah.torabi@rayanhamafza.com>
2025-07-12 14:20:44 +0330

    Define on event emitter to listen to change files

--- index.js
+++ index.js
@@ -7,17 +7,26 @@ import fs from "fs/promises";
 const readCommand = async () => {
 
     const openFileHanlder = await fs.open("./command.txt", "r");
+
     const events = await fs.watch("./command.txt");
+
+    openFileHanlder.on("change", async () => {
+        const fileInfo = await openFileHanlder.stat();
+        const size = await fileInfo.size;
+        const buff = Buffer.alloc(size);
+        const length = buff.byteLength;
+        // the position that we want to start reading the file
+        const position = 0;
+        const offset = 0;
+        // Reading the whole content
+        await openFileHanlder.read(buff, offset, length, position);
+
+        console.log(buff.toString("utf-8"))
+    })
 
     for await (const event of events) {
         if (event.eventType === "change") {
-            const fileInfo = await openFileHanlder.stat();
-            const size = await fileInfo.size;
-            const buff = Buffer.alloc(size);
-            const length = buff.byteLength;
-            const position = 0;
-            const offset = 0;
-            const content = await openFileHanlder.read(buff, offset, length, position);
+            openFileHanlder.emit("change");
         }
     }
 }
