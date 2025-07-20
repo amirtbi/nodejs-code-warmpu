278d44533900096a11e65c9cc878dd20f4ec9984
Amirhosein Torabi <ah.torabi@rayanhamafza.com>
2025-07-20 15:32:10 +0330

    handle copying large file by using fs.open approach

---
+++ copy/copy.js
@@ -0,0 +1,28 @@
+import { read } from "fs";
+import fs from "fs/promises";
+import path from "path";
+
+
+// (async () => {
+//     const srcFileContent = await fs.readFile(path.join(process.cwd(), "copy", "text.txt"));
+//     await fs.writeFile(path.join(process.cwd(), "copy", "copy.txt"), srcFileContent);
+// })()
+
+(async () => {
+
+    try {
+        const srcFile = await fs.open(path.join(process.cwd(), "copy", "text.txt"), "r");
+        const destFile = await fs.open(path.join(process.cwd(), "copy", "copy.txt"), "w");
+        let bytesRead = -1;
+
+        while (bytesRead !== 0) {
+            const srcFileContent = await srcFile.read(); // ready chunk by chunk based on the default buffer
+            console.log("src file", srcFileContent)
+            bytesRead = srcFileContent.bytesRead;
+            await destFile.write(srcFileContent.buffer.slice(0, bytesRead));
+        }
+    }
+    catch (e) {
+        console.log("Errro happened during writing and copying file.")
+    }
+})()
