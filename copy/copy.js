70f1ec27a7764b42be98a0d31a19bf9b543238c9
Amirhosein Torabi <ah.torabi@rayanhamafza.com>
2025-07-21 08:54:46 +0330

    handle streaing using pipe

--- copy/copy.js
+++ copy/copy.js
@@ -1,5 +1,6 @@
 import { read } from "fs";
-import fs from "fs/promises";
+// import fs from "fs/promises";
+import fs from "fs";
 import path from "path";
 
 
@@ -8,21 +9,38 @@ import path from "path";
 //     await fs.writeFile(path.join(process.cwd(), "copy", "copy.txt"), srcFileContent);
 // })()
 
+
+// (async () => {
+
+//     try {
+//         const srcFile = await fs.open(path.join(process.cwd(), "copy", "text.txt"), "r");
+//         const destFile = await fs.open(path.join(process.cwd(), "copy", "copy.txt"), "w");
+//         let bytesRead = -1;
+
+//         while (bytesRead !== 0) {
+//             const srcFileContent = await srcFile.read(); // ready chunk by chunk based on the default buffer
+//             bytesRead = srcFileContent.bytesRead;
+//             await destFile.write(srcFileContent.buffer.slice(0, bytesRead));
+//         }
+//     }
+//     catch (e) {
+//         console.log("Errro happened during writing and copying file.")
+//     }
+// })()
+
+
+// using pipe
 (async () => {
+    const readStream = fs.createReadStream(path.join(process.cwd(), "copy", "text.txt"));
+    const writeStream = fs.createWriteStream(path.join(process.cwd(), "copy", "copy-stream.txt"), { encoding: "utf-8" });
+    readStream.pipe(writeStream);
 
-    try {
-        const srcFile = await fs.open(path.join(process.cwd(), "copy", "text.txt"), "r");
-        const destFile = await fs.open(path.join(process.cwd(), "copy", "copy.txt"), "w");
-        let bytesRead = -1;
+    readStream.on("end", () => {
+        console.log("reading ended")
+    });
 
-        while (bytesRead !== 0) {
-            const srcFileContent = await srcFile.read(); // ready chunk by chunk based on the default buffer
-            console.log("src file", srcFileContent)
-            bytesRead = srcFileContent.bytesRead;
-            await destFile.write(srcFileContent.buffer.slice(0, bytesRead));
-        }
-    }
-    catch (e) {
-        console.log("Errro happened during writing and copying file.")
-    }
+    writeStream.on("finish", () => {
+        console.log("writting finished")
+    });
+
 })()
