bed2eaf9277714bb48537ea17444701005950cd0
Amirhosein Torabi <ah.torabi@rayanhamafza.com>
2025-07-26 09:49:23 +0330

    create a simple TCP server

---
+++ networking/simple-sender.js
@@ -0,0 +1,8 @@
+import net from "net";
+
+const socket = net.createConnection({ host: "127.0.0.1", port: 8000 }, () => {
+    const buff = Buffer.alloc(2);
+    buff[0] = 12;
+    buff[1] = 2;
+    socket.write(buff);
+})
---
+++ networking/simple-server.js
@@ -0,0 +1,12 @@
+import net from "net";
+
+const server = net.createServer((socket) => {
+
+    socket.on("data", (data) => {
+        console.log(data);
+    })
+})
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("opened server on", server.address())
+})
