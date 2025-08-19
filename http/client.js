130ed89b5b050400d0e1c710d4bc26e6036650e3
Amirhosein Torabi <ah.torabi@rayanhamafza.com>
2025-08-19 13:52:15 +0330

    make some warmup codes for http

--- http/client.js
+++ http/client.js
@@ -10,15 +10,24 @@ const request = http.request({
     path: "/create-post",
     headers: {
         "Content-Types": "application/json",
-        "content-length": Buffer.byteLength(JSON.stringify({ message: "hi there" }), "utf-8")
     }
 })
 
 request.on("response", (response) => {
-    console.log("resposne")
+    console.log("----statusCode----", response.statusCode);
+    console.log("----headers----", response.headers);
+    response.on("data", (chunk) => {
+        console.log("----Body----", chunk.toString("utf-8"))
+    })
+
+    response.on("end", () => {
+        console.log("----nore more data and response----")
+    })
 })
 
 
 request.write(JSON.stringify({ message: "hi there" }));
 
+
 request.end(JSON.stringify({ message: "finished" }));
+
--- http/server.js
+++ http/server.js
@@ -5,17 +5,17 @@ const server = http.createServer();
 
 
 server.on("request", (req, res) => {
-    console.log("method", req.method);
-    console.log("url", req.url);
-    console.log("headers", req.headers);
     const chunks = [];
+
     req.on("data", (chunk) => {
-        console.log("Chunk", chunk.toString("utf-8"))
-        chunks.push(chunk);
+        chunks.push(chunk.toString());
     });
 
     req.on("end", () => {
-        res.write(JSON.stringify({ message: "Received", data: chunks }))
+        console.log("data", chunks);
+        res.setHeader("Content-type", "application/json");
+        res.writeHead(200, "ok");
+        res.end(JSON.stringify({ message: "Sent finish flag" }));
     });
 })
 
